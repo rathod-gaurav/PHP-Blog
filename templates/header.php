f847e54f1c8a08cb6d67ba020f478030e25a3112
rathod.gauravvinod@gmail.com <rathod.gauravvinod@gmail.com>
2022-06-17 11:17:25 +0530

    some modifications in css

--- templates/header.php
+++ templates/header.php
@@ -7,49 +7,13 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="main.css">
     <title>PHP Blog</title>
-    <!-- <style>
-        .feedback{
-            color: red;
-        }
-        .homepage-button{
-            padding: 50px 20px 0px 20px;
-            color: khaki;
-        }
-
-        .btn-outline-secondary{
-            border-radius: 20px;
-        }
-
-        .btn-lg{
-            height: 250px;
-            width: inherit;
-        }
-
-        #btn-text{
-            text-decoration: none;
-            color: inherit;
-            font-size: 40px;
-        }
-
-        .card-left{
-            margin-right: 20px
-        }
-
-        .card-right{
-            margin-left: 20px
-        }
-
-        .nav-link{
-            text-decoration: none;
-            color: black;
-        }
-    </style> -->
+    
 </head>
 
 <body class="bg-dark">
-    <header class="jumbotron bg-secondary">
+    <header class="jumbotron">
             <div class="container">
-                <h2 class="text-white text-right">PHP Blog</h2>
+                <h2 class="text-dark text-right">PHP Blog</h2>
             </div>
     </header>
         
